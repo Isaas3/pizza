981cfb5b501fe2749c4809d5c5236c70e76d09eb
Isaas3 <Isaas.Neely@searcyschools.org>
2024-01-30 11:13:30 -0600

    Complete ordering/checkout systems
    choose pizza types and quantity, calculates total cost, and charges the cost through cash or credit.

--- checkout.py
+++ checkout.py
@@ -1,2 +1,37 @@
-def start():
-    print("This is the checkout")
+def start(order):
+    subtotal = 0
+    tax_rate = 0.095
+    print("Customer Order:")
+    for i in order:
+        print(i.quantity, i.size, i.type, i.price)
+        subtotal += i.quantity * i.price
+
+    subtotal = round(subtotal, 2)
+    tax = round(subtotal * tax_rate, 2)
+    total = round(subtotal + tax, 2)
+
+    print(f"Subtotal: ${subtotal}")
+    print(f"Tax: ${tax}")
+    print(f"Total: ${total}")
+
+    payment(total)
+
+    input("(Press ENTER to continue)")
+
+def payment(total):
+    while True:
+        payment_type = input("Cash or credit? ")
+        if payment_type.lower() == "cash":
+            print(f"Your total is ${total}.")
+            cash = int(input("Enter cash recieved: "))
+            change = round(cash - total, 2)
+            print(f"Return ${change} to the customer.")
+            input("(Press ENTER to continue)")
+            break
+        elif payment_type.lower() == "credit":
+            print(f"The total is ${total}.")
+            print("Please swipe the credit card")
+            input("(Press ENTER to continue)")
+            break
+        else:
+            print("Please enter CASH or CREDIT only")
--- main.py
+++ main.py
@@ -3,22 +3,25 @@ Title: Pizza Time
 Description: Manages pizza orders, checkout, and inventory for a local pizza store
 Author: Sam Neely
 """
-
 import order, checkout, inventory
+from os import system
 
-print("Welcome to Pizza Time!")
-print("Select an option below")
-print("1. Order")
-print("2. Checkout")
-print("3. Inventory")
-print("4. Exit")
-
-while True:
+customer_order = []
+while True:    
+    print("Welcome to Pizza Time!")
+    print("Select an option below")
+    print("1. Order")
+    print("2. Checkout")
+    print("3. Inventory")
+    print("4. Exit")
     selection = input(">> ")
     if selection == "1":
-        order.start()
+        customer_order = order.start()
     elif selection == "2":
-        checkout.start()
+        if len(customer_order) > 0:
+            checkout.start(customer_order)
+        else:
+            print("The cart is empty")
     elif selection == "3":
         inventory.start()
     elif selection == "4":
@@ -26,3 +29,6 @@ while True:
         break
     else:
         print("Please enter the correct number for the option you want")
+        input("(Press enter to continue)")
+    
+    system("cls")
